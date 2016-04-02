0be8dec9149a015ef58ec82aea50a7c79eb5cbcd
cxgAux <cxgAux@gmail.com>
2016-04-02 23:07:07 +0800

    <2016/04/02 23:06> buffer initialization remain unimplemented

--- src/Track.hpp
+++ src/Track.hpp
@@ -28,7 +28,55 @@ int Track(int argc, char ** argv) {
             DescInfo _hogInfo = createWith_isHofFlag_cubePartition_xy_t_b_crossSection_w_h(false, __nxy_cells, __nt_cells, 8, __patch_size, __patch_size),
                 _hofInfo = createWith_isHofFlag_cubePartition_xy_t_b_crossSection_w_h(true, __nxy_cells, __nt_cells, 9, __patch_size, __patch_size),
                 _mbhInfo = createWith_isHofFlag_cubePartition_xy_t_b_crossSection_w_h(false, __nxy_cells, __nt_cells, 8, __patch_size, __patch_size);
+            SeqInfo _seqInfo = fromVideo(_capture);
+            cv::Mat _kernelMatrix = createWith_bins_nBins_kernelRadius(__preset_bins, 8, __GaussSmooth);
 
+            if(__toDisplay == true) {
+                cv::namedWindow("Track", cv::WINDOW_AUTOSIZE);
+            }
+
+            //buffers
+            cv::Mat _frame, _image, _prev_gray, _grey;
+
+            //for pixel pyramid generation and restoration
+            std::vector<float> _fscales(0);
+            std::vector<cv::Size> _sizes(0);
+
+            //pixel pyramids
+            std::vector<cv::Mat> _prev_grey_pyr(0), _grey_pyr(0), _flow_pyr(0);
+            //flow pyramids
+            std::vector<cv::Mat> _prev_poly_pyr(0), _poly_pyr(0);
+            //tracker
+            std::vector< std::list<Trajectory> > _xyScaleTracks;
+            //counter to perform sampling
+            int _counter2Sample = 0;
+            while(true) {
+                //get a new frame
+                _capture >> _frame;
+                if(_frame.empty()) {
+                    static int __count = 200;
+                    char * __buff = new char [__count];
+                    /**
+                     *  @warn   <2016/04/02 15:09> bind the videoname as the first trivial argument
+                     */
+                    sprintf(__buff, "Video %s has meet its end!\n", argv[1]);
+                    _log(__buff);
+                    delete [] __buff;
+                    break;
+                }
+
+                if(_frame_idx >= __start_frame && _frame_idx <= __end_frame) {
+                    if(_frame_idx == __start_frame) {
+                        _image.create(_frame.size(), CV_8UC3);
+                        _grey.create(_frame.size(), CV_8UC1);
+                        _prev_gray.create(_frame.size(), CV_8UC1);
+
+                        InitPry(_frame.rows, _frame.cols, _fscales, _sizes);
+                    }
+                }
+
+                _frame_idx ++;
+            }
         }
     }
 }
