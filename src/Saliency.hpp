b2001cd77eae972fecd6628865ba850ae43a6bc1
cxgAux <cxgAux@gmail.com>
2016-04-08 16:30:44 +0800

    <2016/04/08 16:30> motion salinecy done

--- src/Saliency.hpp
+++ src/Saliency.hpp
@@ -4,6 +4,7 @@
 #include "afx.hpp"
 //to test
 #include "VisualProc.hpp"
+#include "Structures.hpp"
 
 float Normalize(cv::Mat & saliencyMap, const int & normRange) {
     int _width = saliencyMap.cols, _height = saliencyMap.rows;
@@ -36,6 +37,10 @@ float Normalize(cv::Mat & saliencyMap, const int & normRange) {
     return _avgSaliency / (_width * _height);
 }
 
+
+/**
+ *  @brief  Apperance Saliency Part
+ */
 void GaussianSmoothC1(const cv::Mat & originImg, cv::Mat & _smoothImg, const std::vector<float> & kernel) {
     int _width = originImg.cols, _height = originImg.rows, _center = int(kernel.size()) / 2, _realIdx = 0;
     _smoothImg.create(_height, _width, CV_32FC1);
@@ -158,5 +163,225 @@ float calculateAppearcanceSaliencyMap(const cv::Mat & grey, cv::Mat & saliencyMa
     }
     return Normalize(saliencyMap, 255);
 }
+
+/**
+ *  @brief  Motion Saliency Part
+ */
+
+ void GaussianSmoothC2(const cv::Mat & originImg, cv::Mat & _smoothImg, const std::vector<float> & kernel) {
+     int _width = originImg.cols, _height = originImg.rows, _center = int(kernel.size()) / 2, _realIdx = 0;
+     _smoothImg.create(_height, _width, CV_32FC2);
+     cv::Mat _buffer(originImg.size(), CV_32FC2);
+     //blur in x direction
+     for(int iHeight = 0; iHeight < _height; ++ iHeight) {
+         for(int iWidth = 0; iWidth < _width; ++ iWidth) {
+             float _xWeightedSum = 0, _yWeightedSum = 0, _kernelSum = 0;
+             const float *pOrigin = originImg.ptr<float>(iHeight);
+             float *pBuffer = _buffer.ptr<float>(iHeight);
+             for(int jWidth = - _center; jWidth <= _center; ++ jWidth) {
+                 _realIdx = jWidth + iWidth;
+                 if(_realIdx >= 0 && _realIdx < _width) {
+                     _xWeightedSum += kernel[_center + jWidth] * pOrigin[2 * _realIdx];
+                     _yWeightedSum += kernel[_center + jWidth] * pOrigin[2 * _realIdx + 1];
+                     _kernelSum += kernel[_center + jWidth];
+                 }
+             }
+             pBuffer[2 * iWidth] = _xWeightedSum / _kernelSum;
+             pBuffer[2 * iWidth + 1] = _yWeightedSum / _kernelSum;
+         }
+     }
+     //blur in y direction
+     for(int iHeight = 0; iHeight < _height; ++ iHeight) {
+         for(int iWidth = 0; iWidth < _width; ++ iWidth) {
+             float _xWeightedSum = 0, _yWeightedSum = 0, _kernelSum = 0;
+             float *pSmooth = _smoothImg.ptr<float>(iHeight);
+             for(int jHeight = - _center; jHeight <= _center; ++ jHeight) {
+                 _realIdx = jHeight + iHeight;
+                 if(_realIdx >= 0 && _realIdx < _height) {
+                     _xWeightedSum += kernel[_center + jHeight] * _buffer.ptr<float>(_realIdx)[2 * iWidth];
+                     _yWeightedSum += kernel[_center + jHeight] * _buffer.ptr<float>(_realIdx)[2 * iWidth + 1];
+                     _kernelSum += kernel[_center + jHeight];
+                 }
+             }
+             pSmooth[2 * iWidth] = _xWeightedSum / _kernelSum;
+             pSmooth[2 * iWidth + 1] = _yWeightedSum / _kernelSum;
+         }
+     }
+ }
+
+ void integral(const cv::Mat & flow, int height, int width, int nBins, const cv::Mat & kernelMatrix, std::vector<float> & intImg) {
+     const float __2pi = 2 * M_PI, _denseBase = __2pi / float(kernelMatrix.rows);
+     int _idx = 0;
+
+     for(int iHeight = 0; iHeight < height; ++ iHeight) {
+         const float * pFlow = flow.ptr<float>(iHeight);
+         std::vector<float> _sum(nBins, 0.f);
+         for(int iWidth = 0; iWidth < width; ++ iWidth, ++ _idx) {
+             float _xflow = pFlow[2 * iWidth], _yflow = pFlow[2 * iWidth + 1], _magnitude = std::sqrt(_xflow * _xflow + _yflow * _yflow);
+             if(_magnitude > __min_flow) {
+                 float _orientation = cv::fastAtan2(_yflow, _xflow);
+                 while(_orientation >= __2pi) {
+                     _orientation -= __2pi;
+                 }
+                 while(_orientation < 0) {
+                     _orientation += __2pi;
+                 }
+
+                 int _iDense = static_cast<int>(cvRound(_orientation / _denseBase));
+                 if(_iDense >= kernelMatrix.rows) {
+                     _iDense = 0;
+                 }
+
+                 //apply kernel histogram
+                 const float * _kernel = kernelMatrix.ptr<float>(_iDense);
+                 for(int iBin = 0; iBin < nBins; ++ iBin) {
+                     _sum[iBin] += _magnitude * _kernel[iBin];
+                 }
+             }
+             int _tmpf = _idx * nBins, _tmps = 0;
+             if(iHeight == 0) {
+                 for(int iBin = 0; iBin < nBins; ++ iBin) {
+                     intImg[_tmpf ++] = _sum[iBin];
+                 }
+             }
+             else {
+                 _tmps = (_idx - width) * nBins;
+                 for(int iBin = 0; iBin < nBins; ++ iBin) {
+                     intImg[_tmpf ++] = intImg[_tmps ++] + _sum[iBin];
+                 }
+             }
+         }
+     }
+ }
+
+void integral(const cv::Mat & xFlow, const cv::Mat & yFlow, int height, int width, int nBins, const cv::Mat & kernelMatrix, std::vector<float> & intImg) {
+    const float __2pi = 2 * M_PI, _denseBase = __2pi / float(kernelMatrix.rows);
+    int _idx = 0;
+
+    for(int iHeight = 0; iHeight < height; ++ iHeight) {
+        const float * pXFlow = xFlow.ptr<float>(iHeight), * pYFlow = yFlow.ptr<float>(iHeight);
+        std::vector<float> _sum(nBins, 0.f);
+        for(int iWidth = 0; iWidth < width; ++ iWidth, ++ _idx) {
+            float _xflow = pXFlow[iWidth], _yflow = pYFlow[iWidth], _magnitude = std::sqrt(_xflow * _xflow + _yflow * _yflow);
+            if(_magnitude > __min_flow) {
+                float _orientation = cv::fastAtan2(_yflow, _xflow);
+                while(_orientation >= __2pi) {
+                    _orientation -= __2pi;
+                }
+                while(_orientation < 0) {
+                    _orientation += __2pi;
+                }
+
+                int _iDense = static_cast<int>(cvRound(_orientation / _denseBase));
+                if(_iDense >= kernelMatrix.rows) {
+                    _iDense = 0;
+                }
+
+                //apply kernel histogram
+                const float * _kernel = kernelMatrix.ptr<float>(_iDense);
+                for(int iBin = 0; iBin < nBins; ++ iBin) {
+                    _sum[iBin] += _magnitude * _kernel[iBin];
+                }
+            }
+            int _tmpf = _idx * nBins, _tmps = 0;
+            if(iHeight == 0) {
+                for(int iBin = 0; iBin < nBins; ++ iBin) {
+                    intImg[_tmpf ++] = _sum[iBin];
+                }
+            }
+            else {
+                _tmps = (_idx - width) * nBins;
+                for(int iBin = 0; iBin < nBins; ++ iBin) {
+                    intImg[_tmpf ++] = intImg[_tmps ++] + _sum[iBin];
+                }
+            }
+        }
+    }
+}
+
+void getIntegralSum(const std::vector<float> & intImg, int xf, int yf, int xs, int ys, int height, int width, int nBins, std::vector<float> & hist) {
+    yf = std::max<int>(0, yf);
+    xf = std::max<int>(0, xf);
+    ys = std::min<int>(height - 1, ys);
+    xs = std::min<int>(width - 1, xs);
+
+    /*
+    (0, 0)         (0, 1)       ....                   (0, width - 1)
+    (1, 0)
+      .
+      .                     (xf, yf)
+      .
+      .                                      (xs, ys)
+      .
+    (height - 1, 0)                                    (height - 1, width - 1)
+    */
+    int _idxTL = ((yf - 1) * width + (xf - 1)) * nBins,/*(xf, yf) is inclusive*/
+        _idxTR = ((yf - 1) * width + xs) * nBins,
+        _idxBL = (ys * width + (xf - 1)) * nBins,
+        _idxBR = (ys * width + xs) * nBins;
+    float _area = (xs - xf + 1) * (ys - yf + 1);
+    for(int iBin = 0; iBin < nBins; ++ iBin) {
+        float _intTL(0), _intTR(0), _intBL(0), _intBR(0);
+        if(yf >= 1) {
+            if(xf >= 1) {
+                _intTL = intImg[_idxTL + iBin];
+            }
+            if(xs >= 0) {
+                _intTR = intImg[_idxTR + iBin];
+            }
+        }
+        if(ys >= 0){
+            if(xf >= 1) {
+                _intBL = intImg[_idxBL + iBin];
+            }
+            if(xs >= 0) {
+                _intBR = intImg[_idxBR + iBin];
+            }
+        }
+        hist[iBin] = _intBR - _intBL - _intTR + _intTL;
+        hist[iBin] = std::max<int>(hist[iBin], 0);
+        hist[iBin] /= _area;
+    }
+}
+
+float calculateMotionSaliencyMap(const cv::Mat & flow, cv::Mat & saliencyMap, const DescInfo & descInfo, const cv::Mat & kernelMatrix) {
+    int _width = flow.cols, _height = flow.rows, _nBins = descInfo._isHof ? descInfo._nBins - 1 : descInfo._nBins;
+    saliencyMap.create(_height, _width, CV_32FC1);
+    std::vector<float> _kernel{1.0f, 2.0f, 1.0f};
+    cv::Mat _smoothFlow;
+    GaussianSmoothC2(flow, _smoothFlow, _kernel);
+    //cv::Mat _smoothXFlow, _smoothYFlow, _flows[2];
+    //cv::split(flow, _flows);
+    //GaussianSmoothC1(_flows[0], _smoothXFlow, _kernel);
+    //GaussianSmoothC1(_flows[1], _smoothYFlow, _kernel);
+    std::vector<float> _intImg(_width * _height * _nBins);
+    integral(_smoothFlow, _height, _width, _nBins, kernelMatrix, _intImg);
+    //integral(_smoothXFlow, _smoothYFlow, _height, _width, _nBins, kernelMatrix, _intImg);
+    //integral(_flows[0], _flows[1], _height, _width, _nBins, kernelMatrix, _intImg);
+    float _averageSaliency = 0;
+    for(int iHeight = 0; iHeight < _height; ++ iHeight) {
+        int _yoff = std::min<int>(iHeight, _height - iHeight);
+        float * _pSaliencyMap = saliencyMap.ptr<float>(iHeight);
+        for(int iWidth = 0; iWidth < _width; ++ iWidth) {
+            int _xoff = std::min<int>(iWidth, _width - iWidth);
+            std::vector<float> _msa(_nBins), _center(_nBins);
+            getIntegralSum(_intImg, iWidth - _xoff, iHeight - _yoff, iWidth + _xoff, iHeight + _yoff, _height, _width, _nBins, _msa);
+            _yoff = std::min<int>(_yoff, 1);
+            _xoff = std::min<int>(_xoff, 1);
+            _pSaliencyMap[iWidth] = 0;
+            getIntegralSum(_intImg, iWidth - _xoff, iHeight - _yoff, iWidth + _xoff, iHeight + _yoff, _height, _width, _nBins, _center);
+            for(int iBin = 0; iBin < _nBins; ++ iBin) {
+                float _sum = _msa[iBin] + _center[iBin], _diff = _msa[iBin] - _center[iBin];
+                _pSaliencyMap[iWidth] += .5f * _diff * _diff / _sum;
+            }
+        }
+    }
+    return Normalize(saliencyMap, 255);
+}
+
+/**
+ *  @brief  Apperance Saliency Part
+ */
+
 
 #endif// ! _SALIENCY_HPP_
